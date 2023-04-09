bc4ec240d4a8e239dc0b1705655a633fc457f003
Lucas Charoing <charoing.lucas@gmail.com>
2023-04-09 15:38:31 +0200

    Add lightsbaer text and change layout

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ const button = document.querySelector(".button");
 
 button.addEventListener("click", () => {
   if (on) {
-    gsap.to(".light-saber-light", {height: 0, duration: 0.5});
+    gsap.to(".light-saber-light", {width: '0%', duration: 0.5});
     on = false;
   } else {
-    gsap.to(".light-saber-light", {height: 500, duration: 0.5});
+    gsap.to(".light-saber-light", {width: '100%', duration: 0.5});
     on = true;
   }
 });
+
+const animateButton = () => {
+  gsap.to(".button .center", {duration: 0.3, opacity: 0.3});
+  gsap.to(".button .center", {duration: 0.5, delay: 0.6, opacity: 1});
+  gsap.to(".button .circle", {duration: 0.5, delay: 0.6,opacity: 0, scale: 1.3});
+  gsap.to(".button .circle", {duration: 0, delay: 1.2, scale: 1});
+  gsap.to(".button .circle", {duration: 1, delay: 1.5, opacity: 1});
+  setTimeout(() => {
+    animateButton();
+  }, 4000);
+}
+
+animateButton();
